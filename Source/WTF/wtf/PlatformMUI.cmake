d32e9c1ef82c186b111364688ae79ad5403349e2
Krzysztof Smiechowicz <deadwood@wp.pl>
2016-03-21 19:06:57 +0100

    Source/WTF: use pthreads to implement WTF threading support
    pthreads are already use for C++11 threading support, so there is
    no point in keeping two separate threading implementations in
    Odyssey. Also the custom thread specific code was not working and
    mixing thread specific from pthreads with customg threading code
    was dangerous.
    
    To be merged to cumulative patch.

--- Source/WTF/wtf/PlatformMUI.cmake
+++ Source/WTF/wtf/PlatformMUI.cmake
@@ -2,6 +2,6 @@ list(APPEND WTF_SOURCES
     mui/MainThreadMUI.cpp
     mui/execallocator.cpp
     OSAllocatorAROS.cpp
-    ThreadSpecificMorphOS.cpp
-    ThreadingMorphOS.cpp
+    ThreadingPthreads.cpp
+    ThreadIdentifierDataPthreads.cpp
 )
