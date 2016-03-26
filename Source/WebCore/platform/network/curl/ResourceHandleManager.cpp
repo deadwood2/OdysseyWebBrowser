fa8ebd66867ef21d0f1ab243b25d8ef90dc990a3
Krzysztof Smiechowicz <deadwood@wp.pl>
2016-03-26 15:13:45 +0100

    [ODSS] Source/WebCore: fixes to disk cache logic
    - when saving index, save only fully downloaded objects
    - if download is canceled, invalidate the cache entry, else partially
    downloaded files can be used for next request

--- Source/WebCore/platform/network/curl/ResourceHandleManager.cpp
+++ Source/WebCore/platform/network/curl/ResourceHandleManager.cpp
@@ -875,6 +875,7 @@ void ResourceHandleManager::downloadTimerCallback()
         ASSERT(d->m_handle == handle);
 
         if (d->m_cancelled) {
+            CurlCacheManager::getInstance().didCancel(*job);
             removeFromCurl(job);
             continue;
         }
@@ -890,6 +891,7 @@ void ResourceHandleManager::downloadTimerCallback()
             if (!d->m_response.responseFired()) {
                 handleLocalReceiveResponse(d->m_handle, job, d);
                 if (d->m_cancelled) {
+                    CurlCacheManager::getInstance().didCancel(*job);
                     removeFromCurl(job);
                     continue;
                 }
