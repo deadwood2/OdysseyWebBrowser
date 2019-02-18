cdb660896bde3eeb27e386057c6cf71102b9086f
jaokim <joakim.nordstrom@gmail.com>
2019-02-18 22:03:08 +0100

    Added ifs for alll our platforms.

--- Source/WTF/wtf/PlatformMUI.cmake
+++ Source/WTF/wtf/PlatformMUI.cmake
@@ -1,7 +1,21 @@
 list(APPEND WTF_SOURCES
     mui/MainThreadMUI.cpp
+)
+if (${WTF_OS_AROS})
+list(APPEND WTF_SOURCES
     mui/execallocator.cpp
     OSAllocatorAROS.cpp
+)
+elif (${WTF_OS_AMIGAOS4})
+list(APPEND WTF_SOURCES
+    OSAllocatorMorphOS.cpp
+)
+elif (${WTF_OS_MORPHOS})
+list(APPEND WTF_SOURCES
+    OSAllocatorMorphOS.cpp
+)
+endif()
+list(APPEND WTF_SOURCES
     ThreadingPthreads.cpp
     ThreadIdentifierDataPthreads.cpp
 )
