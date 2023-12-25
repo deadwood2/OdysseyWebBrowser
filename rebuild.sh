2b82a522be129d0ef0f41a37d22c9e190ef20924
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2023-12-25 10:35:41 +0100

    Add configuration for 64-bit build

--- rebuild.sh
+++ rebuild.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-AROS_DEVELOPMENT=/ssd/deadwood/repo-github-dd-alt-abiv0/cross-i386-aros/Development
-
 show_selection ()
 {
     printf "    1)  i386-aros\n"
+    printf "    2)  x86_64-aros\n"
 }
 
 process_selection ()
 {
     case $1 in
         1)
-        BUILD_DIR=$(pwd)/cross-build-i386-aros
+        BUILD_PROCESSOR=i386
+        SDK_DIR=/ssd/deadwood/repo-github-dd-alt-abiv0/cross-$BUILD_PROCESSOR-aros/Development
+        ;;
+        2)
+        BUILD_PROCESSOR=x86_64
+        SDK_DIR=/ssd/deadwood/repo-github-dd-core/cross-$BUILD_PROCESSOR-aros/Development
         ;;
     esac
+
+    BUILD_DIR=$(pwd)/cross-build-$BUILD_PROCESSOR-aros
 }
 
 
@@ -36,16 +42,18 @@ main ()
     cd $BUILD_DIR
 
 
+
     cmake \
-        -DCMAKE_CROSSCOMPILING=ON \
         -DCMAKE_TOOLCHAIN_FILE=../Source/cmake/AROS.cmake \
         -DCMAKE_EXE_LINKER_FLAGS=-static-libstdc++ \
         -DCMAKE_BUILD_TYPE=Release \
         -DPORT=MUI \
-        -DCMAKE_SYSTEM_PROCESSOR=i386 \
-        -DCAIRO_INCLUDE_DIRS=$AROS_DEVELOPMENT/include/cairo/ \
-        -DFREETYPE_INCLUDE_DIRS=$AROS_DEVELOPMENT/include/freetype/ \
-        -DLIBXML2_INCLUDE_DIR=$AROS_DEVELOPMENT/include/libxml2/ \
+        -DCMAKE_SYSTEM_PROCESSOR=$BUILD_PROCESSOR \
+        -DCMAKE_C_COMPILER=$BUILD_PROCESSOR-aros-gcc \
+        -DCMAKE_CXX_COMPILER=$BUILD_PROCESSOR-aros-g++ \
+        -DCAIRO_INCLUDE_DIRS=$SDK_DIR/include/cairo/ \
+        -DFREETYPE_INCLUDE_DIRS=$SDK_DIR/include/freetype/ \
+        -DLIBXML2_INCLUDE_DIR=$SDK_DIR/include/libxml2/ \
        ..
 }
 
