6ff977f0b045b6ee6a12859f907bac9f396c0081
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2024-01-29 10:11:52 +0100

    Update version information

--- Source/WebKit/mui/Api/MorphOS/versioninfo.h
+++ Source/WebKit/mui/Api/MorphOS/versioninfo.h
@@ -1,7 +1,7 @@
 #ifndef __VERSIONINFO_H__
 #define __VERSIONINFO_H__
 
-#define VERSION  "1.25"
+#define VERSION  "1.26"
 #define WEBKITVER xstringify(WEBKIT_MAJOR_VERSION) "." xstringify(WEBKIT_MINOR_VERSION)
 #define REVISION "WebKit " WEBKITVER " (r187682)"
 
