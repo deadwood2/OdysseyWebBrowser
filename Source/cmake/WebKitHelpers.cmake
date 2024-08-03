7b04fc7aabb9d896da23f3fb4262c2bcbcd14441
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2024-08-03 18:10:59 +0200

    [ODSS] Fixes to make 64-bit version compile

--- Source/cmake/WebKitHelpers.cmake
+++ Source/cmake/WebKitHelpers.cmake
@@ -25,7 +25,10 @@ macro(WEBKIT_SET_EXTRA_COMPILER_FLAGS _target)
             set(OLD_COMPILE_FLAGS "-Wno-missing-field-initializers ${OLD_COMPILE_FLAGS}")
         endif ()
 
-        set(OLD_COMPILE_FLAGS "-march=i686 ${OLD_COMPILE_FLAGS}")
+        # Architecture specific compile flags
+        if ("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "i386")
+            set(OLD_COMPILE_FLAGS "-march=i686 ${OLD_COMPILE_FLAGS}")
+        endif ()
 
         # Enable warnings by default
         if (NOT ${OPTION_IGNORECXX_WARNINGS})
