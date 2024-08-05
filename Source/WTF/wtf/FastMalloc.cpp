1e2be4a3ef0c5bc0a3ae8df947caa00129ee21e6
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2024-08-05 09:01:42 +0200

    [ODSS] Source/WTF: use execallocator for fastAlignedMalloc
    Using directly posix_memalign causes more allocation that needed due to
    how posix_memalign operates on AROS. Execallocator is more optimal.

--- Source/WTF/wtf/FastMalloc.cpp
+++ Source/WTF/wtf/FastMalloc.cpp
@@ -112,6 +112,7 @@ TryMallocReturnValue tryFastZeroedMalloc(size_t n)
 #include <malloc.h>
 #elif OS(AROS)
 #include "mui/arosbailout.h"
+#include "mui/execallocator.h"
 #elif OS(MORPHOS)
 #include <clib/debug_protos.h>
 #endif
@@ -161,7 +162,12 @@ void* fastAlignedMalloc(size_t alignment, size_t size)
     ASSERT_IS_WITHIN_LIMIT(size);
 retry:
     void* p = nullptr;
+#if OS(AROS)
+    p = allocator_getmem_aligned(size, alignment);
+#else
     posix_memalign(&p, alignment, size ? size : 2);
+#endif
+
     if (UNLIKELY(!p))
     {
 #if OS(AROS)
@@ -177,6 +183,7 @@ retry:
             goto retry;
 #endif
     }
+
     return p;
 }
 
@@ -184,13 +191,21 @@ void* tryFastAlignedMalloc(size_t alignment, size_t size)
 {
     FAIL_IF_EXCEEDS_LIMIT(size);
     void* p = nullptr;
+#if OS(AROS)
+    p = allocator_getmem_aligned(size, alignment);
+#else
     posix_memalign(&p, alignment, size);
+#endif
     return p;
 }
 
 void fastAlignedFree(void* p) 
 {
+#if OS(AROS)
+    allocator_freemem(p);
+#else
     free(p);
+#endif
 }
 
 #endif // OS(WINDOWS)
