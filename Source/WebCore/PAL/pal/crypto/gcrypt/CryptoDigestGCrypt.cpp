9076750492b379233dcb086469f82819e6c1be71
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2024-08-06 06:42:29 +0200

    [ODSS] Broken CryptoDigestGnuTLS so that executable links

--- Source/WebCore/PAL/pal/crypto/gcrypt/CryptoDigestGCrypt.cpp
+++ Source/WebCore/PAL/pal/crypto/gcrypt/CryptoDigestGCrypt.cpp
@@ -27,13 +27,13 @@
 #include "config.h"
 #include "CryptoDigest.h"
 
-#include <gcrypt.h>
+//#include <gcrypt.h>
 
 namespace PAL {
 
 struct CryptoDigestContext {
     int algorithm;
-    gcry_md_hd_t md;
+//    gcry_md_hd_t md;
 };
 
 CryptoDigest::CryptoDigest()
@@ -49,46 +49,46 @@ std::unique_ptr<CryptoDigest> CryptoDigest::create(CryptoDigest::Algorithm algor
 
     switch (algorithm) {
     case CryptoDigest::Algorithm::SHA_1:
-        gcryptAlgorithm = GCRY_MD_SHA1;
+//        gcryptAlgorithm = GCRY_MD_SHA1;
         break;
     case CryptoDigest::Algorithm::SHA_224:
-        gcryptAlgorithm = GCRY_MD_SHA224;
+//        gcryptAlgorithm = GCRY_MD_SHA224;
         break;
     case CryptoDigest::Algorithm::SHA_256:
-        gcryptAlgorithm = GCRY_MD_SHA256;
+//        gcryptAlgorithm = GCRY_MD_SHA256;
         break;
     case CryptoDigest::Algorithm::SHA_384:
-        gcryptAlgorithm = GCRY_MD_SHA384;
+//        gcryptAlgorithm = GCRY_MD_SHA384;
         break;
     case CryptoDigest::Algorithm::SHA_512:
-        gcryptAlgorithm = GCRY_MD_SHA512;
+//        gcryptAlgorithm = GCRY_MD_SHA512;
         break;
     }
 
     std::unique_ptr<CryptoDigest> digest(new CryptoDigest);
     digest->m_context->algorithm = gcryptAlgorithm;
 
-    gcry_md_open(&digest->m_context->md, gcryptAlgorithm, 0);
-    if (!digest->m_context->md)
-        return nullptr;
-
+//    gcry_md_open(&digest->m_context->md, gcryptAlgorithm, 0);
+//    if (!digest->m_context->md)
+//        return nullptr;
+//
     return digest;
 }
 
 void CryptoDigest::addBytes(const void* input, size_t length)
 {
-    gcry_md_write(m_context->md, input, length);
+//    gcry_md_write(m_context->md, input, length);
 }
 
 Vector<uint8_t> CryptoDigest::computeHash()
 {
-    int digestLen = gcry_md_get_algo_dlen(m_context->algorithm);
-    Vector<uint8_t> result(digestLen);
+//    int digestLen = gcry_md_get_algo_dlen(m_context->algorithm);
+    Vector<uint8_t> result;//(digestLen);
 
-    gcry_md_final(m_context->md);
-    memcpy(result.data(), gcry_md_read(m_context->md, 0), digestLen);
-    gcry_md_close(m_context->md);
-
+//    gcry_md_final(m_context->md);
+//    memcpy(result.data(), gcry_md_read(m_context->md, 0), digestLen);
+//    gcry_md_close(m_context->md);
+//
     return result;
 }
 
