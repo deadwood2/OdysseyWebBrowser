c8d82d44e6fa0fb74875c7c9122214ec7f0bebc5
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2024-08-06 06:41:59 +0200

    [ODSS] Change default heap size to 196MB for x86_64
    Make sure OWB works on weaker configs

--- Source/JavaScriptCore/jit/ExecutableAllocator.cpp
+++ Source/JavaScriptCore/jit/ExecutableAllocator.cpp
@@ -98,7 +98,7 @@ static const size_t fixedExecutableMemoryPoolSize = 16 * 1024 * 1024;
 #elif CPU(ARM64)
 static const size_t fixedExecutableMemoryPoolSize = 128 * 1024 * 1024;
 #elif CPU(X86_64)
-static const size_t fixedExecutableMemoryPoolSize = 1024 * 1024 * 1024;
+static const size_t fixedExecutableMemoryPoolSize = 196 * 1024 * 1024;
 #else
 static const size_t fixedExecutableMemoryPoolSize = 32 * 1024 * 1024;
 #endif
