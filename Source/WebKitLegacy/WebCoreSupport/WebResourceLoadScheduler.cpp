e7b4645fffe302be00a170a59ee423ba6ce696e5
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2024-08-07 17:48:49 +0200

    [ODSS] Source/WebKit: cumulative changes

--- Source/WebKitLegacy/WebCoreSupport/WebResourceLoadScheduler.cpp
+++ Source/WebKitLegacy/WebCoreSupport/WebResourceLoadScheduler.cpp
@@ -21,7 +21,7 @@
     the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
     Boston, MA 02110-1301, USA.
  */
-
+#include "config.h"
 #include "WebResourceLoadScheduler.h"
 
 #include "PingHandle.h"
