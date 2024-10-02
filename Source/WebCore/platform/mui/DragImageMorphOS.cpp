8b257eefadfe265c3185318e02c04c0a48db0165
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2024-10-02 17:55:21 +0200

    Update to 2.24 code base
    Copied from DragImageGtk.cpp. Solves double-free of cairo_surface_t

--- Source/WebCore/platform/mui/DragImageMorphOS.cpp
+++ Source/WebCore/platform/mui/DragImageMorphOS.cpp
@@ -1,5 +1,5 @@
 /*
- * 2010 Igalia S.L
+ *  Copyright (C) 2010,2017 Igalia S.L.
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
@@ -38,44 +38,47 @@ namespace WebCore {
 IntSize dragImageSize(DragImageRef image)
 {
     if (image)
-        return IntSize(cairo_image_surface_get_width((cairo_surface_t*)image.get()), cairo_image_surface_get_height((cairo_surface_t *)image.get()));
+        return { cairo_image_surface_get_width(image.get()), cairo_image_surface_get_height(image.get()) };
 
-    return IntSize(0, 0);
+    return { 0, 0 };
 }
 
-void deleteDragImage(DragImageRef image)
+void deleteDragImage(DragImageRef)
 {
-    if (image)
-        cairo_surface_destroy((cairo_surface_t *)image.get());
+    // Since this is a RefPtr, there's nothing additional we need to do to
+    // delete it. It will be released when it falls out of scope.
 }
 
 DragImageRef scaleDragImage(DragImageRef image, FloatSize scale)
 {
     if (!image)
-        return 0;
+        return nullptr;
 
-    int newWidth = scale.width() * cairo_image_surface_get_width((cairo_surface_t *)image.get());
-    int newHeight = scale.height() * cairo_image_surface_get_height((cairo_surface_t *)image.get());
-    cairo_surface_t* scaledSurface = cairo_surface_create_similar((cairo_surface_t *)image.get(), CAIRO_CONTENT_COLOR_ALPHA, newWidth, newHeight);
+    IntSize imageSize = dragImageSize(image);
+    IntSize scaledSize(imageSize);
+    scaledSize.scale(scale.width(), scale.height());
+    if (imageSize == scaledSize)
+        return image;
 
-    RefPtr<cairo_t> context = adoptRef(cairo_create(scaledSurface));
+    RefPtr<cairo_surface_t> scaledSurface = adoptRef(cairo_surface_create_similar(image.get(), CAIRO_CONTENT_COLOR_ALPHA, scaledSize.width(), scaledSize.height()));
+
+    RefPtr<cairo_t> context = adoptRef(cairo_create(scaledSurface.get()));
     cairo_scale(context.get(), scale.width(), scale.height());
     cairo_pattern_set_extend(cairo_get_source(context.get()), CAIRO_EXTEND_PAD);
     cairo_pattern_set_filter(cairo_get_source(context.get()), CAIRO_FILTER_BEST);
     cairo_set_operator(context.get(), CAIRO_OPERATOR_SOURCE);
-    cairo_set_source_surface(context.get(), (cairo_surface_t *)image.get(), 0, 0);
+    cairo_set_source_surface(context.get(), image.get(), 0, 0);
     cairo_paint(context.get());
 
-    deleteDragImage((cairo_surface_t *)image.get());
     return scaledSurface;
 }
 
 DragImageRef dissolveDragImageToFraction(DragImageRef image, float fraction)
 {
     if (!image)
-        return 0;
+        return nullptr;
 
-    RefPtr<cairo_t> context = adoptRef(cairo_create((cairo_surface_t *)image.get()));
+    RefPtr<cairo_t> context = adoptRef(cairo_create(image.get()));
     cairo_set_operator(context.get(), CAIRO_OPERATOR_DEST_IN);
     cairo_set_source_rgba(context.get(), 0, 0, 0, fraction);
     cairo_paint(context.get());
@@ -84,18 +87,16 @@ DragImageRef dissolveDragImageToFraction(DragImageRef image, float fraction)
 
 DragImageRef createDragImageFromImage(Image* image, ImageOrientationDescription)
 {
-    return image->nativeImageForCurrentFrame().leakRef();
+    return image->nativeImageForCurrentFrame();
 }
 
 DragImageRef createDragImageIconForCachedImageFilename(const String&)
 {
-    return 0;
+    return nullptr;
 }
 
-DragImageRef createDragImageForLink(Element& element, URL& url, const String& inLabel, TextIndicatorData&, FontRenderingMode fontRenderingMode, float)
+DragImageRef createDragImageForLink(Element& element, URL& url, const String&, TextIndicatorData&, FontRenderingMode fontRenderingMode, float)
 {
-    UNUSED_PARAM(inLabel)
-
     D(kprintf("createDragImageForLink %s %s\n", url.string().latin1().data(), inLabel.latin1().data()));
 
     FrameView *frameView = element.document().frame()->view();
@@ -105,7 +106,7 @@ DragImageRef createDragImageForLink(Element& element, URL& url, const String& in
     {
         set(widget->browser, MA_OWBBrowser_DragURL, url.string().latin1().data());
     }
-    return 0;
+    return nullptr;
 }
 
 DragImageRef createDragImageForColor(const Color&, const FloatRect&, float, Path&)
