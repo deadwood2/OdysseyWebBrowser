ce2c8261629bc3ab713bfa8b6abad68acf030015
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2025-04-08 08:34:58 +0200

    [ODSS] Source/WebCore: fixes to disk cache logic
    - when saving index, save only fully downloaded objects
    - if download is canceled, invalidate the cache entry, else partially
    downloaded files can be used for next request

--- Source/WebCore/platform/network/curl/CurlRequest.cpp
+++ Source/WebCore/platform/network/curl/CurlRequest.cpp
@@ -489,6 +489,11 @@ void CurlRequest::didCompleteTransfer(CURLcode result)
 void CurlRequest::didCancelTransfer()
 {
     finalizeTransfer();
+#if PLATFORM(MUI)
+    callClient([this, protectedThis = makeRef(*this)](CurlRequest& request, CurlRequestClient& client) {
+        client.curlDidCancel(request);
+    });
+#endif
     cleanupDownloadFile();
 }
 
